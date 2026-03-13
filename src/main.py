bb967d4ecd8d45ac1fb7b322e033f0b64c6293ab
Aleksandr64ru <q400w@mail.ru>
2026-03-13 18:39:24 +0400

    Ver.2.Bot.change.token

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
-import asyncio
+import os
 
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
 
 
-BOT_TOKEN = "8787403562:AAGGv750U61jl9jzEwn9FfnREOZPnir4BEI"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 bot = Bot(token=BOT_TOKEN)
@@ -14,6 +15,12 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def handle_start(message: Message):
     await message.answer("Hello!")
+    
+async def handler(event: dict, context):
+    print(f"{event}")
+    print(f"{context}")
+    return{"statusCode": 200, "body": ""}  
+    
     
 async def main():
     await dp.start_polling(bot)
